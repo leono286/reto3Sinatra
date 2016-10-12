b818cc8c8f4e93ca12bdb965542140938ae5e196
Leonardo Pérez M <leono286@gmail.com>
2016-10-11 20:57:25 -0500

    Modificado para segundo reto

--- solution.rb
+++ solution.rb
@@ -1,9 +1,5 @@
 require "sinatra"
 
-get '/' do
-	unless params[:nombre]
-		"<h1>Hola desconocido!</h1>"
-	else
-		"<h1>Hola #{params[:nombre]}!</h1>"
-	end
+get '/makers/:nombre' do
+		"<h1>Hola #{params[:nombre].capitalize}!</h1>"
 end
